190912f6a4daf59f55574c41e0e0e02d2b6de552
Gilles Vink <gilles@meneervink.nl>
2022-08-23 13:52:52 +0200

    Added license limit and formatted with black

--- deadline-balancer.py
+++ deadline-balancer.py
@@ -14,45 +14,69 @@ def CleanupDeadlineEventListener(deadlinePlugin):
 
 class DeadlineBalancer(DeadlineEventListener):
     def __init__(self):
-        self.OnJobSubmittedCallback += self.OnJobSubmitted
-        self.OnHouseCleaningCallback += self.OnHouseCleaning
+        self.OnJobSubmittedCallback += self.on_job_submitted
+        self.OnHouseCleaningCallback += self.on_house_cleaning
 
     def Cleanup(self):
         del self.OnJobSubmittedCallback
         del self.OnHouseCleaningCallback
 
-    def OnJobSubmitted(self, job):
+    def on_job_submitted(self, job):
+        license_limit = job.GetJobPluginInfoKeyValue("LicenseLimit")
+        if license_limit:
+            RepositoryUtils.SetMachineLimitMaximum(
+                job.JobId, int(license_limit)
+            )
         self.Balance()
 
-    def OnHouseCleaning(self):
-        self.Balance()
+    def on_house_cleaning(self):
+        """Will perform on house cleaning a balance for
+        all jobs.
+        """
+        self.balance()
 
-    def Balance(self):
+    def balance(self):
+        """Will calculate for all jobs the amount of machines
+        that can render the job depending on the priority.
+
+        For example: 2 jobs are rendering. We have 10 machines.
+        Job 1 has a priority of 90, job 2 has a priority of 10.
+
+        Job 1 will get 9 machines, job 2 will get 1 machine.
+
+        When a license limit is active, the job will not get any
+        more pc's than the active license limit.
+        """
         print("Starting balancing of current active jobs")
 
         # function variables
-        totalPriority = 0.0
-        skipJobs = []
-        reusableWorkers = 0
+        total_priority = 0.0
+        skip_jobs = []
+        reusable_workers = 0
 
         # get all active jobs
         jobs = RepositoryUtils.GetJobsInState("Active")
-        jobsN = len(jobs)
+        jobs_amount = len(jobs)
 
-        print("Total amount of jobs balancing: %s" % (jobsN))
+        print("Total amount of jobs balancing: %s" % (jobs_amount))
 
         # get all active workers
-        workersUnfiltered = RepositoryUtils.GetSlaveInfoSettings(invalidateCache=True)
+        workers_unfiltered = RepositoryUtils.GetSlaveInfoSettings(
+            invalidateCache=True
+        )
         workers = []
 
         # filter workers for active only
-        for worker in workersUnfiltered:
+        for worker in workers_unfiltered:
             if worker.Info.SlaveIsActive:
                 workers.append(worker)
 
-        workersN = len(workers)
+        workers_amount = len(workers)
 
-        print("Total amount of active workers for balancing: %s" % (workersN))
+        print(
+            "Total amount of active workers for balancing: %s"
+            % (workers_amount)
+        )
 
         # set the total priority for all jobs found
         for job in jobs:
@@ -60,51 +84,79 @@ class DeadlineBalancer(DeadlineEventListener):
             priority = job.JobPriority
 
             # update total priority
-            totalPriority += priority
+            total_priority += priority
 
-        print("The total priority is: %s" % totalPriority)
+        print("The total priority is: %s" % total_priority)
 
         # calculate the machine limit for every job
         for job in jobs:
 
             # calculate the amount of workers applicable for this job
             percent = 0.0
-            percent = float(job.JobPriority) / float(totalPriority)
+            percent = float(job.JobPriority) / float(total_priority)
 
             # if no slaves are available set the amount to 1 to avoid errors
-            if math.ceil(percent * workersN) is 0:
+            if math.ceil(percent * workers_amount) is 0:
                 workers = 1
             else:
-                workers = math.ceil(percent * workersN)
+                workers = math.ceil(percent * workers_amount)
+
+            license_limit = job.GetJobPluginInfoKeyValue("LicenseLimit")
+
+            if license_limit:
+                license_limit = int(license_limit)
+                if workers >= license_limit:
+                    workers = license_limit
+                    print(
+                        "Job %s has a license limit, will limit to %s"
+                        % (job, str(license_limit))
+                    )
 
             print("Handing %s over to %s" % (workers, job))
             RepositoryUtils.SetMachineLimitMaximum(job.JobId, workers)
             job.MachineLimit = workers
 
-            # check if job has rendering frames that are less than applicable amount of workers
+            # check if job has rendering frames that are less than
+            # applicable amount of workers
             frames = job.JobRenderingTasks + job.JobQueuedTasks
             if frames < workers:
-                skipJobs.append(job)
-                reusableWorkers += workers - frames
+                skip_jobs.append(job)
+                reusable_workers += workers - frames
                 workers = frames
                 print(
-                    "Detected fewer frames than handed workers. Handing %s over to %s"
-                    % (workers, job)
+                    "Detected fewer frames than handed workers. "
+                    "Handing %s over to %s" % (workers, job)
                 )
                 job.MachineLimit = workers
                 RepositoryUtils.SetMachineLimitMaximum(job.JobId, workers)
 
         # redistribute reusable workers
         for job in jobs:
-            if job in skipJobs:
+            if job in skip_jobs:
                 continue
 
-            if reusableWorkers > 0:
+            if reusable_workers > 0:
                 # calculate the new amount
-                percent = float(job.JobPriority) / float(totalPriority)
-                workers = math.ceil(percent * reusableWorkers)
+                percent = float(job.JobPriority) / float(total_priority)
+                workers = math.ceil(percent * reusable_workers)
 
-                print("Handing %s extra workers to %s" % (workers, job))
-                RepositoryUtils.SetMachineLimitMaximum(
-                    job.JobId, job.MachineLimit + workers
-                )
+                # Detect if job has a machine limit
+                license_limit = job.GetJobPluginInfoKeyValue("LicenseLimit")
+                if license_limit:
+                    license_limit = int(license_limit)
+                    new_machine_limit = job.MachineLimit + workers
+                    if license_limit >= new_machine_limit:
+                        RepositoryUtils.SetMachineLimitMaximum(
+                            job.JobId, job.MachineLimit + workers
+                        )
+                    else:
+                        print(
+                            "Will not add extra machines because "
+                            "of license limit"
+                        )
+
+                else:
+                    print("Handing %s extra workers to %s" % (workers, job))
+                    RepositoryUtils.SetMachineLimitMaximum(
+                        job.JobId, job.MachineLimit + workers
+                    )
